d2c8d3bd5fc166c89e8db1821b027144b35c5d09
ScottishCat <ZHS56@pitt.edu>
2019-12-10 22:28:32 -0500

    Final

---
+++ precache-manifest.859d68a907e2d1cc0a6e629d633ce023.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec4aa95c014e3980c333a6670162bea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "93bce97098cd609ce012",
+    "url": "./static/css/main.33c127ab.chunk.css"
+  },
+  {
+    "revision": "bc277802bf30a23fbb6b",
+    "url": "./static/js/2.6521c0b9.chunk.js"
+  },
+  {
+    "revision": "93bce97098cd609ce012",
+    "url": "./static/js/main.16233c9a.chunk.js"
+  },
+  {
+    "revision": "1d2ad6de650b9de52806",
+    "url": "./static/js/runtime-main.b8edfdb7.js"
+  },
+  {
+    "revision": "121a1131d0a1043f0b315873b9e43d18",
+    "url": "./static/media/Active Life.121a1131.png"
+  },
+  {
+    "revision": "35699303061ba8652b21b9162f2f5f13",
+    "url": "./static/media/BG.35699303.jpg"
+  },
+  {
+    "revision": "b436ee048a09499778a842d641bb5e28",
+    "url": "./static/media/Beauty & Spas.b436ee04.png"
+  },
+  {
+    "revision": "25e25809d59d89408ff935ff4031e254",
+    "url": "./static/media/Las Vegas.25e25809.png"
+  },
+  {
+    "revision": "b9971c7414035e9be256cc336e3218a9",
+    "url": "./static/media/Phoenix.b9971c74.png"
+  },
+  {
+    "revision": "69e61aca08ff57848b9c91e5d313da4a",
+    "url": "./static/media/Pittsburgh.69e61aca.png"
+  },
+  {
+    "revision": "7b80fe5be5a41fc05f8418befb2cfafd",
+    "url": "./static/media/Restaurants.7b80fe5b.png"
+  },
+  {
+    "revision": "02ccfac2b208d6be24f19ebbc45a0355",
+    "url": "./static/media/Shopping.02ccfac2.png"
+  },
+  {
+    "revision": "717e2f23074871f2fbae8fb3807ad3dc",
+    "url": "./static/media/Toronto.717e2f23.png"
+  },
+  {
+    "revision": "6c3eef21dcc3691391c2d12fe3386de2",
+    "url": "./static/media/bike.6c3eef21.jpg"
+  },
+  {
+    "revision": "3e62c017c753b76c8bf82dac8bf23c8d",
+    "url": "./static/media/boss.3e62c017.png"
+  },
+  {
+    "revision": "910bb34b88dc5ee97d0121020dcb6179",
+    "url": "./static/media/breakfast.910bb34b.jpg"
+  },
+  {
+    "revision": "40c2a6fbd8d7a1d559fa3e19a31869d5",
+    "url": "./static/media/burgers.40c2a6fb.jpg"
+  },
+  {
+    "revision": "55516532eacab9ce658e2af7e6206cff",
+    "url": "./static/media/camera.55516532.jpg"
+  },
+  {
+    "revision": "e665ce6aca40fe3723e92163ba96c65b",
+    "url": "./static/media/cocktail.e665ce6a.png"
+  },
+  {
+    "revision": "67c0d9dfdcc46dd34ef3ab78b187f0bb",
+    "url": "./static/media/hats.67c0d9df.jpg"
+  },
+  {
+    "revision": "47d8181c9b6210f51f1e1c72c21617e2",
+    "url": "./static/media/honey.47d8181c.jpg"
+  },
+  {
+    "revision": "f0fd32dd146594209fb09b85b5d25864",
+    "url": "./static/media/morning.f0fd32dd.jpg"
+  },
+  {
+    "revision": "f5080759f593ca466a5bcc8c815b0132",
+    "url": "./static/media/mushroom.f5080759.jpg"
+  },
+  {
+    "revision": "82de811f338bac5be242a5ff204fe5f0",
+    "url": "./static/media/olive.82de811f.jpg"
+  },
+  {
+    "revision": "e62e7305a38167ce35e000156e8cbda8",
+    "url": "./static/media/plant.e62e7305.jpg"
+  },
+  {
+    "revision": "3058d9063295ef2246ef8f68cde4a580",
+    "url": "./static/media/refuel.3058d906.png"
+  },
+  {
+    "revision": "28648bdad07256d84109adb0b3999eda",
+    "url": "./static/media/star.28648bda.jpg"
+  },
+  {
+    "revision": "4827316c67179957091c835f7c590cc7",
+    "url": "./static/media/tools.4827316c.png"
+  },
+  {
+    "revision": "9ed88304eaa080ccf9873af25ce15807",
+    "url": "./static/media/vegetables.9ed88304.jpg"
+  }
+]);
